2b80462ed5595b0c7a55400d3f6f749a732e0302
Rohit Kulkarni <rohit.kulkarni@realmile.com>
2015-12-04 14:27:06 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by rohitkulkarni on 04/12/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hey This is just a test for jenkins. Blah blah !! by RK");
+    }
+}
